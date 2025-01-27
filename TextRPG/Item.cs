785d39c01e58340b9e40be2d89cac1965ae07ba3
jj930220 <jj930220@gmail.com>
2025-01-27 21:05:27 +0900

    아이템 설정중

---
+++ TextRPG/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    enum ITEMTYPE
+    {
+        EQUIP=1,
+        CONSUME
+    }
+
+    interface IItem
+    {
+        public string Name { get; }
+
+        public ITEMTYPE ITEMTYPE { get; }
+
+        public void use(ITEMTYPE type)
+        {
+            switch(type)
+            {
+                case ITEMTYPE.EQUIP:
+                    break;
+                case ITEMTYPE.CONSUME:
+                    break;
+            }
+        }
+
+
+
+    }
+}
